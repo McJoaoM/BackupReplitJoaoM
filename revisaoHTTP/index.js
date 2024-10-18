2ec267eeba5502600fa567f614f141f803642c65
McJoaoM <130366173+McJoaoM@users.noreply.github.com>
2024-10-18 07:47:42 -0300

    Add files via upload

---
+++ revisaoHTTP/index.js
@@ -0,0 +1,12 @@
+// IMPORTANDO MODULO COM REQUIRE
+const http = require ('http');
+
+const PORT = 8000 // PORTA DO SERVIDOR
+//CRIANDO O SERVIDOR HTTP
+const servidor = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+});
+// INICIANDO O SERVIDOR NA PORTA 8000
+servidor.listen(PORT, () => {
+  console.log (`Servidor rodando na porta ${PORT}`);  
+}); 
